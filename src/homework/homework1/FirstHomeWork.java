5e09ab3053902ee0930c9f07c78e9f65fab13e19
Jora Petrosyan <petrosyan.jora10@gmail.com>
2025-09-09 02:16:14 +0400

    first commit

---
+++ src/homework/homework1/FirstHomeWork.java
@@ -0,0 +1,25 @@
+package homework.homework1;
+
+public class FirstHomeWork {
+    public static void main(String[] args) {
+
+        int x = 7;
+        int y = 10;
+
+        if (y < x) {
+            System.out.println(y + "is more then" + x);
+        }
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+
+        int a = 5;
+        int b = 7;
+        System.out.println(a + b);
+
+        int n = 3;
+        for (int i = 1; i < 10; i++) {
+            System.out.println(n + "*" + i + "=" + n * i);
+        }
+    }
+}
